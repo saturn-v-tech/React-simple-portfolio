89369bca3d113e88c9ee5e62a77ebcd7508f82a4
saturn-v-tech <fredrmog@stud.ntnu.no>
2021-10-17 18:20:24 +0200

    info

--- src/projectData.js
+++ src/projectData.js
@@ -1,6 +1,6 @@
 const projects = [
   {
-    title: 'CodeSync',
+    title: 'Master thesis: Determinants for venture capital performance in Scandinavia',
     image: './images/project1.png',
     description: "Bulding SaaS product for help student and teacher's Community, Web App include Real time Chat, Audio, CodeShare, WhiteBoard and Many more!",
     tools: ['ReactJs', 'NodeJs', 'ExpressJs', "MongoDB", "Redux","SocketIO","WebRTC", "JWT" , "Google Auth"],
@@ -8,7 +8,7 @@ const projects = [
     // link: 'https://github.com/Code-Deeper/code-sync',
   },
   {
-    title: 'Patang Store',
+    title: 'Master thesis: How the EU Taxonomy influences the construction industrys transition from a linear to a circular economy',
     image: './images/project2-768.png',
     description: 'Full featured ecommerce store built with MERN stack and Paypal payments API. Admin page to manage customers, products & orders.',
     tools: ['React', 'Redux', 'MongoDB', 'JWT', 'Express'],
