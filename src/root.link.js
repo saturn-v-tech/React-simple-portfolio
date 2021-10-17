d1bbacd0557db961a0a66e70d2f098657075000b
saturn-v-tech <fredrmog@stud.ntnu.no>
2021-10-17 18:16:49 +0200

    kontaktinfo

--- src/root.link.js
+++ src/root.link.js
@@ -1,11 +1,11 @@
 export const RESUME_PATH = './Vivekkumar_Javiya_Resume.pdf';
-export const GITHUB_USERNAME = 'codewithvk';
-export const LINKEDIN_USERNAME = 'vivekkumarjaviya';
-export const FIRST_NAME = 'Vivekkumar';
-export const LAST_NAME = 'Javiya';
-export const ADDRESS = 'Rajkot, Gujrat'
-export const EMAIL_ID = 'work.vivekjaviya@gmail.com';
-export const TWITTER_ID = 'Vk_javiya';
-export const FRONT_PAGE_NAME = 'Vivek Kumar';
-export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer & Open Source Contributor.I like to build things for web and solving problems.'
+export const GITHUB_USERNAME = 'saturn-v-tech';
+export const LINKEDIN_USERNAME = 'fredrikmoger';
+export const FIRST_NAME = 'Fredrik';
+export const LAST_NAME = 'Moger';
+export const ADDRESS = 'Schøyensgate 10 A, Trondheim'
+export const EMAIL_ID = 'fredrik.moger@gmail.com';
+export const TWITTER_ID = 'fredrikmoger';
+export const FRONT_PAGE_NAME = 'Fredrik Moger';
+export const FRONT_PAGE_DESC = 'Engineer and entreprenuer. I like to build things and solve problems.'
 
